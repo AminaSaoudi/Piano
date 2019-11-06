29acc521d892812d36a4da86e9e4472b016c390b
AminaSaoudi <56294662+AminaSaoudi@users.noreply.github.com>
2019-11-06 14:45:00 +0100

    Add files via upload

---
+++ frequencies.py
@@ -0,0 +1,50 @@
+# https://fr.wikipedia.org/wiki/Note_de_musique
+
+import sqlite3
+connect = sqlite3.connect("frequencies.db")
+cursor = connect.cursor()
+cursor.execute("DROP TABLE IF EXISTS frequencies")
+cursor.execute("CREATE TABLE frequencies ( \
+                    octave INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
+                    C float,\
+                    CSharp float,\
+                    D  float,\
+                    DSharp float,\
+                    E float,\
+                    F float,\
+                    FSharp float,\
+                    G float,\
+                    GSharp float,\
+                    A      float,\
+                    ASharp float,\
+                    B float\
+);")
+
+
+# f0=440.0
+
+# frequencies=[]
+# octave=[]
+# octave.append(3)
+# for j in range (-9,3) :
+#     frequency=f0*2**(j/12.0)
+#     octave.append(frequency)
+# frequencies.append(octave)
+
+# cursor.executemany("INSERT INTO frequencies VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);", frequencies)
+# connect.commit()
+f0=55.0
+
+frequencies=[]
+for i in range(-1,10):
+    octave=[]
+    octave.append(i)
+    f=f0*2**(i)
+    for j in range (-9,3) :
+        frequency=f*2**(j/12.0)
+        frequency=format(frequency,'.2f')
+        octave.append(frequency)
+    frequencies.append(octave)
+print(frequencies[0])
+cursor.executemany("INSERT INTO frequencies VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);", frequencies)
+connect.commit()
